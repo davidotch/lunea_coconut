383440aea93deed81b695dbadcd325b721ad46e2
David Cardon <da.cardon@outlook.fr>
2023-06-12 18:04:00 +0200

    create, import navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css'
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Gallery from './pages/gallery'
+import Contact from './pages/contact'
 
 function App() {
     return (
-        <div>
-            <h1>my app</h1>
-        </div>
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gallery() {
+    return (
+        <div className="products">
+            <h1>Gallerie</h1>
+        </div>
+    )
+}
+
+export default Gallery
