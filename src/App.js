f7522818e2eec38b7ef6387d5657dcb6d201104f
David Cardon <da.cardon@outlook.fr>
2023-08-04 17:48:59 +0200

    fix route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import SectionOne from './pages/home'
 import Gallery from './pages/gallery'
 import Contact from './pages/contact'
+import Footer from './components/footer/footer'
 
 function App() {
     return (
@@ -11,10 +12,11 @@ function App() {
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<SectionOne />} />
                     <Route path="/gallery" element={<Gallery />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </>
     )
