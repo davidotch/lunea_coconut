486fe39a31c19adf309b114d84e905217c903a10
David Cardon <da.cardon@outlook.fr>
2023-08-23 18:24:30 +0200

    create homepage.js

---
+++ src/pages/homePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from '../components/home/home'
+import '../style/home.css'
+
+export default function homePage() {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
