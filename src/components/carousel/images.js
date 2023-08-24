436cd8bcabaac10aa2868047918f8b7fbe56fd8b
David Cardon <da.cardon@outlook.fr>
2023-08-24 19:32:41 +0200

    fix access cover url

---
+++ src/components/carousel/images.js
@@ -0,0 +1,58 @@
+const Images = [
+    {
+        id: 1,
+        cover: 'images/background-home.jpg',
+        alt: 'Image 1',
+        title: 'bouddha',
+    },
+    {
+        id: 2,
+        cover: 'images/mer.jpg',
+        alt: 'Image 2 ',
+        title: 'Paysage marin',
+    },
+    {
+        id: 3,
+        cover: './images/oiseau.jpg',
+        alt: 'Image 3',
+        title: 'cui-cui le petit oiseau',
+    },
+    {
+        id: 4,
+        cover: 'https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=452&q=80',
+        alt: 'Image 4',
+        title: 'Lamborghini Aventador SV',
+    },
+    {
+        id: 5,
+        cover: 'https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        alt: 'Image 5',
+        title: 'Ferrari 458 Speciale',
+    },
+    {
+        id: 6,
+        cover: 'https://images.unsplash.com/photo-1471479917193-f00955256257?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80',
+        alt: 'Image 6',
+        title: 'Porsche 911',
+    },
+    {
+        id: 7,
+        cover: 'https://images.unsplash.com/photo-1555353540-64580b51c258?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=378&q=80',
+        alt: 'Image 7',
+        title: 'Dodge Challenger',
+    },
+    {
+        id: 8,
+        cover: 'https://i.pinimg.com/750x/88/33/1b/88331be20045f95b28e91e21fa663ad0.jpg',
+        alt: 'Image 8',
+        title: 'Lamborghini Gallardo',
+    },
+    {
+        id: 9,
+        cover: 'https://i.pinimg.com/564x/2e/40/02/2e40027b9b156589cfbccbf7b33d3bc7.jpg',
+        alt: 'Image 9',
+        title: '2021 Mercedes-AMG GLE53 Coupe electrifies',
+    },
+]
+
+export default Images
