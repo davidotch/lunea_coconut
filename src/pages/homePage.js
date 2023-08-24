669e6660db9ba9632302d6a61d624063cba06486
David Cardon <da.cardon@outlook.fr>
2023-08-24 19:33:58 +0200

    import carousel

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from '../components/home/home'
+import Carousel from '../components/carousel/carousel'
 import '../style/home.css'
 
 export default function homePage() {
     return (
         <>
             <Home />
+            <Carousel />
         </>
     )
 }
