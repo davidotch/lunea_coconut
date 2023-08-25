76448d76ad351095d1abc92725ebea9165b03ee2
David Cardon <da.cardon@outlook.fr>
2023-08-25 19:31:53 +0200

    fix routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from './components/navbar/navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/homePage'
-import Gallery from './pages/gallery'
-import Contact from './pages/contact'
 import Footer from './components/footer/footer'
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/gallery" element={<Gallery />} />
-                    <Route path="/contact" element={<Contact />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
