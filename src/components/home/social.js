5e0b4c3bc94b80ba002d6217a8342e57d77aef8a
David Cardon <da.cardon@outlook.fr>
2023-08-26 23:09:56 +0200

    create social array

---
+++ src/components/home/social.js
@@ -0,0 +1,22 @@
+import * as FaIcons from 'react-icons/fa'
+
+export const SocialList = [
+    {
+        title: 'Facebook',
+        path: 'https://www.facebook.com/',
+        cName: 'socialLi',
+        icon: <FaIcons.FaFacebook color="#fff" size={30} />,
+    },
+    {
+        title: 'Instagram',
+        path: '/',
+        cName: 'socialLi',
+        icon: <FaIcons.FaInstagram color="#fff" size={30} />,
+    },
+    {
+        title: '  Contact',
+        path: '/',
+        cName: 'socialLi',
+        icon: <FaIcons.FaEnvelope color="#fff" size={30} />,
+    },
+]
