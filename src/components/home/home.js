2b150231f8486aff747a9c9e3efc9b96925a9b01
David Cardon <da.cardon@outlook.fr>
2023-08-26 23:08:41 +0200

    add SocialList.map

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,24 @@
 import React from 'react'
+import { SocialList } from '../home/social'
 import '../../style/home.css'
 
 export default function home() {
-    return <main className="home"></main>
+    return (
+        <main className="home">
+            <div className="social">
+                <ul>
+                    {SocialList.map((item, index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <a href={item.path}>
+                                    <span>{item.title}</span>
+                                    {item.icon}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </main>
+    )
 }
