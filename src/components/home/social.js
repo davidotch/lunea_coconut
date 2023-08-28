28e7d92228b53eca7a5b17fd5906499a44086ef6
David Cardon <da.cardon@outlook.fr>
2023-08-28 19:57:33 +0200

    fix path

--- src/components/home/social.js
+++ src/components/home/social.js
@@ -3,20 +3,20 @@ import * as FaIcons from 'react-icons/fa'
 export const SocialList = [
     {
         title: 'Facebook',
-        path: 'https://www.facebook.com/',
+        path: 'https://www.facebook.com/profile.php?id=100093448566259',
         cName: 'social-list',
         icon: <FaIcons.FaFacebook color="#fff" size={30} />,
     },
     {
         title: 'Instagram',
-        path: 'https://www.instagram.com/',
+        path: 'https://www.instagram.com/luneaetcoconut/',
         cName: 'social-list',
         icon: <FaIcons.FaInstagram color="#fff" size={30} />,
     },
-    {
-        title: 'Contact',
-        path: 'contact',
-        cName: 'social-list',
-        icon: <FaIcons.FaEnvelope color="#fff" size={30} />,
-    },
+    // {
+    //     title: 'Contact',
+    //     path: 'mailto:luneaetcoconut@hotmail.com',
+    //     cName: 'social-list',
+    //     icon: <FaIcons.FaEnvelope color="#fff" size={30} />,
+    // },
 ]
