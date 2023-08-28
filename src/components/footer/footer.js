4172e10254f654781cfc3b1fd9c3ab6e8831bead
David Cardon <da.cardon@outlook.fr>
2023-08-28 20:19:53 +0200

    add color react-icon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -15,7 +15,7 @@ export default function footer() {
                         href="https://www.facebook.com/profile.php?id=100093448566259"
                         target="_blank"
                         rel="noreferrer">
-                        <FaFacebookSquare size={35} />
+                        <FaFacebookSquare size={35} color="#3b5998" />
                     </a>
                 </li>
                 <li>
@@ -23,12 +23,12 @@ export default function footer() {
                         href="https://www.instagram.com/luneaetcoconut/"
                         target="_blank"
                         rel="noreferrer">
-                        <FaInstagramSquare size={35} />
+                        <FaInstagramSquare size={35} color="#c13584" />
                     </a>
                 </li>
                 <li>
                     <a href="mailto:luneaetcoconut@hotmail.com">
-                        <FaEnvelopeSquare size={35} />
+                        <FaEnvelopeSquare size={35} color="#6fc2b0" />
                     </a>
                 </li>
             </ul>
