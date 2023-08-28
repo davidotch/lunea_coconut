6db9aed8065f87038c05a270a498a1b520bb045f
David Cardon <da.cardon@outlook.fr>
2023-08-28 19:41:18 +0200

    add routes Gallery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/homePage'
+import Gallery from './pages/gallery'
 import Footer from './components/footer/footer'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/gallery" element={<Gallery />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
