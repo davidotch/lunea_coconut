06f5eae6c0b25cc96f081197ffa0b1a8cfb2975d
DavyDotch <da.cardon@outlook.fr>
2023-11-24 17:22:00 +0100

    update

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -7,94 +7,67 @@ import '../style/carousel.css'
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 const Carousel = () => {
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [seletedImage, setSelectedImage] = useState(null)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [selectedImage, setSelectedImage] = useState(null)
 
-    const handleOpenModal = (index) => {
-        setSelectedImage(Images[currentIndex + index])
-    }
+  const handleOpenModal = (index) => {
+    setSelectedImage(Images[currentIndex + index])
+  }
 
-    const handleCloseModal = () => {
-        setSelectedImage(null)
-    }
+  const handleCloseModal = () => {
+    setSelectedImage(null)
+  }
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 3) % Images.length)
-    }
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 3) % Images.length)
+  }
 
-    const handlePrev = () => {
-        setCurrentIndex(
-            (prevIndex) => (prevIndex - 3 + Images.length) % Images.length
-        )
-    }
-
-    const variants = {
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-    }
-
-    return (
-        <div className="content">
-            <div className="container" id="carousel">
-                <h1>Mes Créations</h1>
-                <motion.div
-                    initial="initial"
-                    animate="animate"
-                    variants={variants}
-                    transition={{ duration: 0.5 }}>
-                    <div className="carousel-container">
-                        {Images.slice(currentIndex, currentIndex + 3).map(
-                            (item, index) => (
-                                <motion.div
-                                    key={item.id}
-                                    className="img-container"
-                                    onClick={() => handleOpenModal(index)}>
-                                    <motion.img
-                                        src={item.cover}
-                                        alt={item.alt}
-                                        className="img"
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{ duration: 0.5 }}
-                                    />
-                                    <motion.p
-                                        className="description"
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{ duration: 0.5 }}>
-                                        {item.title}
-                                    </motion.p>
-                                    <motion.p
-                                        className="description"
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{ duration: 0.5 }}>
-                                        {item.description}
-                                    </motion.p>
-                                </motion.div>
-                            )
-                        )}
-                    </div>
-                    <button
-                        className="carousel-button prev-button"
-                        onClick={handlePrev}>
-                        <FaArrowLeft />
-                    </button>
-                    <button
-                        className="carousel-button next-button"
-                        onClick={handleNext}>
-                        <FaArrowRight />
-                    </button>
-                </motion.div>
-            </div>
-            {seletedImage && (
-                <Modal
-                    selectedImage={seletedImage}
-                    onClose={handleCloseModal}
-                />
-            )}
-        </div>
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 3 + Images.length) % Images.length
     )
+  }
+
+  return (
+    <div className="carousel-content">
+      <div className="container" id="carousel">
+        <h1>Mes Créations</h1>
+        <div>
+          <div className="carousel-container">
+            {Images.slice(currentIndex, currentIndex + 3).map((item, index) => (
+              <motion.div
+                key={item.id}
+                className="img-container"
+                onClick={() => handleOpenModal(index)}>
+                <motion.img
+                  src={item.cover}
+                  alt={item.alt}
+                  className="img"
+                  initial={{ opacity: 0.5 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1 }}
+                />
+                <div className="carousel-hover">
+                  <p>Aperçu</p>
+                </div>
+                <p className="description">{item.title}</p>
+                <p className="description">{item.description}</p>
+              </motion.div>
+            ))}
+          </div>
+          <button className="carousel-button prev-button" onClick={handlePrev}>
+            <FaArrowLeft />
+          </button>
+          <button className="carousel-button next-button" onClick={handleNext}>
+            <FaArrowRight />
+          </button>
+        </div>
+      </div>
+      {selectedImage && (
+        <Modal selectedImage={selectedImage} onClose={handleCloseModal} />
+      )}
+    </div>
+  )
 }
 
 export default Carousel
--- src/components/subComponents/card.js
+++ src/components/subComponents/card.js
@@ -2,13 +2,16 @@ import React from 'react'
 import '../../style/card.css'
 
 const Card = ({ cover, title, description }) => {
-    return (
-        <div className="card-content">
-            <img src={cover} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </div>
-    )
+  return (
+    <div className="card-content">
+      <img src={cover} alt={title} />
+      <div className="card-hover">
+        <p>Aperçu</p>
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  )
 }
 
 export default Card
