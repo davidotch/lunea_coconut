ec4ee662e894025d33b95303b4006a7cf224931b
DavyDotch <da.cardon@outlook.fr>
2023-11-28 11:01:12 +0100

    rename

---
+++ src/components/datas/imagesCarousel.js
@@ -0,0 +1,52 @@
+const Images = [
+    {
+        id: 1,
+        cover: '../canvy/Bright_modern_kitchen_interior.jpg',
+        alt: 'Image 1',
+        title: 'Vague',
+        hovered: '../images/vague.jpg',
+        description: '30 x 40',
+    },
+    {
+        id: 2,
+        cover: '../canvy/Bright_studio_apartment_living_room.jpg',
+        alt: 'Image 2 ',
+        title: 'Paysage marin',
+        hovered: '../images/mer.jpg',
+        description: '30 x 40',
+    },
+    {
+        id: 3,
+        cover: '../canvy/Dining_area_with_wooden_furniture.jpg',
+        alt: 'Image 3',
+        title: 'Ponton',
+        hovered: '../images/ponton.jpg',
+        description: '30 x 40',
+    },
+    {
+        id: 4,
+        cover: '../canvy/Scandinavian_dining_room.jpg',
+        alt: 'Image 4',
+        title: 'Bouddha',
+        hovered: '../images/buddha.jpg',
+        description: '30 x 40',
+    },
+    {
+        id: 5,
+        cover: '../canvy/The_kids_playroom.jpg',
+        alt: 'Image 5',
+        title: 'Chardonneret',
+        hovered: '../images/oiseau.jpg',
+        description: '30 x 40',
+    },
+    {
+        id: 6,
+        cover: '../canvy/Comfy_bedroom_with_side_tables_and_lamps.jpg',
+        alt: 'Image 6',
+        title: 'Jour et Nuit',
+        hovered: '../images/arbre_jour_nuit.jpg',
+        description: '30 x 40',
+    },
+]
+
+export default Images
---
+++ src/components/datas/imagesShop.js
@@ -0,0 +1,25 @@
+const Images = [
+  {
+    id: 1,
+    title: 'Palmier',
+    cover: '../canvy/3xtableau.jpg',
+    hovered: '../images/palmier.jpg',
+    description: '30 x   40',
+  },
+  {
+    id: 2,
+    title: "jeune fille au bord de l'eau",
+    cover: '../canvy/decorJeuneFille.jpg',
+    hovered: '../images/jeuneFille.jpg',
+    description: '30 x 40',
+  },
+  {
+    id: 3,
+    title: 'Montagne',
+    cover: '../canvy/decorMontagneMarine.jpg',
+    hovered: '../images/montagneMarine.jpg',
+    description: '30 x 40',
+  },
+]
+
+export default Images
