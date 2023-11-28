a4f4fe6edfa6df5c336969542ebfae761ce11cd8
DavyDotch <da.cardon@outlook.fr>
2023-11-28 11:02:35 +0100

    create modal

--- src/components/shop.js
+++ src/components/shop.js
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './subComponents/card'
 import '../style/shop.css'
 import Transition from '../transition'
+import Modal from '../components/subComponents/modal'
 
-import Images from './datas/imageShop'
+import Images from './datas/imagesShop'
 
-const shop = () => {
-    return (
-        <div className="shop-content">
-            <h1>Nostalgie d'été</h1>
-            <div className="shop-card">
-                {Images &&
-                    Images.map((item) => (
-                        <Card
-                            key={item.id}
-                            cover={item.cover}
-                            title={item.title}
-                            description={item.description}
-                        />
-                    ))}
-            </div>
-        </div>
-    )
+const Shop = () => {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const handleOpenModal = (index) => {
+    setSelectedImage(Images[index])
+  }
+
+  const handleCloseModal = () => {
+    setSelectedImage(null)
+  }
+
+  return (
+    <div className="shop-content">
+      <h1>Nostalgie d'été</h1>
+      <div className="shop-card">
+        {Images &&
+          Images.map((item, index) => (
+            <Card
+              key={index}
+              onClick={() => handleOpenModal(index)}
+              cover={item.cover}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+      </div>
+      {selectedImage && (
+        <Modal selectedImage={selectedImage} onClose={handleCloseModal} />
+      )}
+    </div>
+  )
 }
 
-export default Transition(shop)
+export default Transition(Shop)
