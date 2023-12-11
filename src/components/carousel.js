9da93bcaa7a6bb09430dfe9291f6b0ed6bea0a55
DavyDotch <da.cardon@outlook.fr>
2023-12-11 15:02:32 +0100

    add responsive

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import Modal from './subComponents/modal'
 import Images from './datas/imagesCarousel'
@@ -9,6 +9,24 @@ import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [selectedImage, setSelectedImage] = useState(null)
+  const [imagesToShow, setImagesToShow] = useState(3)
+
+  useEffect(() => {
+    const updateImagesToShow = () => {
+      if (window.innerWidth < 480) {
+        setImagesToShow(1)
+      } else if (window.innerWidth < 940) {
+        setImagesToShow(2)
+      } else {
+        setImagesToShow(3)
+      }
+    }
+
+    window.addEventListener('resize', updateImagesToShow)
+    updateImagesToShow()
+
+    return () => window.removeEventListener('resize', updateImagesToShow)
+  }, [])
 
   const handleOpenModal = (index) => {
     setSelectedImage(Images[currentIndex + index])
@@ -19,12 +37,12 @@ const Carousel = () => {
   }
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 3) % Images.length)
+    setCurrentIndex((prevIndex) => (prevIndex + imagesToShow) % Images.length)
   }
 
   const handlePrev = () => {
     setCurrentIndex(
-      (prevIndex) => (prevIndex - 3 + Images.length) % Images.length
+      (prevIndex) => (prevIndex - imagesToShow + Images.length) % Images.length
     )
   }
 
@@ -34,26 +52,28 @@ const Carousel = () => {
         <h1>Mes Créations</h1>
         <div>
           <div className="carousel-container">
-            {Images.slice(currentIndex, currentIndex + 3).map((item, index) => (
-              <motion.div
-                key={item.id}
-                className="img-container"
-                onClick={() => handleOpenModal(index)}>
-                <motion.img
-                  src={item.cover}
-                  alt={item.alt}
-                  className="img"
-                  initial={{ opacity: 0.5 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 1 }}
-                />
-                <div className="carousel-hover">
-                  <p>Aperçu</p>
-                </div>
-                <p className="description">{item.title}</p>
-                <p className="description">{item.description}</p>
-              </motion.div>
-            ))}
+            {Images.slice(currentIndex, currentIndex + imagesToShow).map(
+              (item, index) => (
+                <motion.div
+                  key={item.id}
+                  className="img-container"
+                  onClick={() => handleOpenModal(index)}>
+                  <motion.img
+                    src={item.cover}
+                    alt={item.alt}
+                    className="img"
+                    initial={{ opacity: 0.5 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1 }}
+                  />
+                  <div className="carousel-hover">
+                    <p>Aperçu</p>
+                  </div>
+                  <p className="description">{item.title}</p>
+                  <p className="description">{item.description}</p>
+                </motion.div>
+              )
+            )}
           </div>
           <button className="carousel-button prev-button" onClick={handlePrev}>
             <FaArrowLeft />
